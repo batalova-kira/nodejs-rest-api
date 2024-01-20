fa670d4e8d1afb6d8981d1b291f013013026911c
batalova-kira <batalovakira@gmail.com>
2024-01-20 15:30:00 +0200

    bug-verificationToken

--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ const userSchema = new Schema(
         },
         verificationToken: {
             type: String,
-            required: [true, "Verify token is required"],
+            // required: [true, "Verify token is required"],
         },
         token: String,
     },
